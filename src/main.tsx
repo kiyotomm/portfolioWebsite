d30b376b349b72b7d2153c294e9f55f5c6a1dc8e
Tom <kiyotomm99@gmail.com>
2024-11-30 07:48:54 +0900

    added react  helmet

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,17 @@ import "./index.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import router from "./components/routes.tsx";
 import { RouterProvider } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
+
 // import theme from "./theme.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ChakraProvider>
       {/* <ColorModeScript initialColorMode={theme.config.initialColorMode} /> */}
-      <RouterProvider router={router} />
+      <HelmetProvider>
+        <RouterProvider router={router} />
+      </HelmetProvider>
     </ChakraProvider>
   </StrictMode>
 );
