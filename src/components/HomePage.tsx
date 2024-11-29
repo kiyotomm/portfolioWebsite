3d535bdee2e125e561601d916728c88dde14691e
Tom <kiyotomm99@gmail.com>
2024-11-30 08:11:12 +0900

    sitemap

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -7,8 +7,9 @@ const HomePage = () => {
     <div className="h-[50rem] w-[99vw] dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center py-[] mt-10">
       <Helmet>
         <title>kiyotomm</title>
-        <meta name="kiyotomm" content="kiyotomm homepage" />
+        <meta name="description" content="kiyotomm homepage" />
         <meta name="keywords" content="homepage, react, seo" />
+        <link href="/" />
       </Helmet>
       {/* Radial gradient for the container to give a faded look */}
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
