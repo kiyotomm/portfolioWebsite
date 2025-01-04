6d0523270cb0ebc39f1c00f39d9eb01d47cf4291
kiyodesu <kiyotomm99@gmail.com>
2025-01-04 17:50:18 +0900

    updated copyright

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,10 @@
 import { Text, VStack } from "@chakra-ui/react";
 
 const Footer = () => {
+  const date = new Date();
   return (
     <VStack>
-      <Text as="b">© 2024 kiyotomm. All rights reserved.</Text>
+      <Text as="b">© {date.getFullYear()} kiyotomm. All rights reserved.</Text>
       <Text>"walang kukulong tubig sa patay sindi na kalan"</Text>
     </VStack>
   );
